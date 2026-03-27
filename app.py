6dd2709d48b4e51e7a180a4b2b9971c1fcbf95a5
Adel Ouled Said <ouledsa221@campus.unicaen.fr>
2026-03-27 21:45:15 +0100

    fix database table creation on render

--- app.py
+++ app.py
@@ -752,7 +752,8 @@ def test_telegram():
 # =========================
 # RUN
 # =========================
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
